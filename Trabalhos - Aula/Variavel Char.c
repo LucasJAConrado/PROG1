de54682885258614fd6c408e361c0677dce2acb3
LucasJAConrado <32078991+LucasJAConrado@users.noreply.github.com>
2017-10-16 18:54:55 -0200

    Add files via upload

---
+++ Trabalhos - Aula/Variavel Char.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+
+{
+    
+char nome[20];
+
+printf("Qual o seu nome: ");
+scanf("%s",nome);
+
+printf("\n\nBoa noite %s\n\n",nome);
+
+
+system("pause");
+return 0;
+
+
+}    
+    
