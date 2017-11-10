791af835ec28901477dc43e35f3b832d9f694163
LucasJAConrado <32078991+LucasJAConrado@users.noreply.github.com>
2017-11-10 21:05:34 -0200

    Add files via upload

---
+++ Trabalhos - Aula/Forca.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+
+{
+ 
+char palavras [5][20]={
+"computador",
+"livro",
+"teclado",
+"agua",
+"lixeira"};
+char letra; 
+
+printf("Entre com a primeira letra: ");
+scanf("%c", &letra);
+
+for(i=0;i<=19;i++)
+  {
+     if(palavras[0][i]==letra)
+       {
+                                                     
+
+             
+                               
+    
+    
+system("pause");    
+return 0;    
+}    
